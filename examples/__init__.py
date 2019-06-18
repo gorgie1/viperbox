d96ec45a01e9d933bb327ce0465af7c86056a86c
Viper Vit <vitolg1@gmail.com>
2019-06-18 16:14:18 -0400

    examples moved.

--- examples/__init__.py
+++
@@ -1,5 +0,0 @@
-from ..viperbox.viperdriver.src.core import SessionDriver as SeleniumDriver
-from ..viperbox.viperlib.src.jsd import jsondata as datafile
-from ..viperbox import logger as logger
-from ..viperbox.viperdriver import logger as driver_logger
-from ..viperbox.viperdriver import PATH_SELF as this_module_location
---
+++ viperbox/viperdriver/examples/__init__.py
@@ -0,0 +1,4 @@
+from ..src.core import SessionDriver as SeleniumDriver
+from ...viperlib.src.jsd import jsondata as datafile
+from .. import logger as driver_logger
+from .. import PATH_SELF as this_module_location
