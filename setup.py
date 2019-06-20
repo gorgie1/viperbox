3e19583c204b6ca99bd22df72b1831c50bd71aae
Viper Vit <vitolg1@gmail.com>
2019-06-20 19:29:31 -0400

    Revert "tests deliberately set to fail (to test)"
    This reverts commit 3d44543f923b15f09d41016e4007d676ac820954.

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author_email='vitolg1@gmail.com',
     licence='MIT',
     description='Collection of tools and libraries including custom expansion of Selenium WebDriver.',
-    version='0.29',
+    version='0.28',
     packages=find_packages()
 )
--- viperbox/viperlib/tests/test_viperlib.py
+++ viperbox/viperlib/tests/test_viperlib.py
@@ -7,4 +7,4 @@ def test_dir_get():
     # test for this module directory
     assert dir_get(__file__) == this_dir
     # test for this module parent's directory
-    assert dir_get(os.path.dirname(parent.__file__)) != dir_get(this_dir)
+    assert dir_get(os.path.dirname(parent.__file__)) == dir_get(this_dir)
