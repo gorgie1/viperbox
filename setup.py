3d44543f923b15f09d41016e4007d676ac820954
Viper Vit <vitolg1@gmail.com>
2019-06-20 19:28:53 -0400

    tests deliberately set to fail (to test)

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author_email='vitolg1@gmail.com',
     licence='MIT',
     description='Collection of tools and libraries including custom expansion of Selenium WebDriver.',
-    version='0.28',
+    version='0.29',
     packages=find_packages()
 )
