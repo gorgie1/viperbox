fa208f0ef51e98d65a4a2f21556a9e847aaa5be7
Viper Vit <vitolg1@gmail.com>
2019-06-20 19:30:52 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     author_email='vitolg1@gmail.com',
     licence='MIT',
     description='Collection of tools and libraries including custom expansion of Selenium WebDriver.',
-    version='0.28',
+    version='0.30',
     packages=find_packages()
 )
