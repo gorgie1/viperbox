84635ca8c06d801a844e55022a969801c39bfa37
gorgie1 <51165330+gorgie1@users.noreply.github.com>
2019-07-02 17:30:33 -0400

    Update __init__.py
    Remove switch_to_window() (add as a method of SessionDriver instead)

--- viperbox/vebdriver/__init__.py
+++ viperbox/vebdriver/__init__.py
@@ -10,14 +10,6 @@ PATH_SELF = __path__[0]
 
 KWD_DIR_HOME = 'home'
 
-def switch_to_window(drv, s, strict=False): # strict mode if True: title must match exaclty
-   rc = False
-   for handle in drv.window_handles:
-       drv.switch_to_window(handle)
-       if (strict and drv.title == title) or (not strict and s in drv.title):
-          rc = drv.title
-          return rc
-
 def dropdown_all_options_list_get(drv, elementId):
     lst = []
     items = drv.find_elements_by_xpath('//select[@id=\'' + elementId + '\']/option')
